a1172fdcef9f43c4b03c72a22ae5226626e8b734
chachamaru6 <kotaro.tech0130@gmail.com>
2021-09-30 14:09:08 +0900

    アニメーション練習

--- main.js
+++ main.js
@@ -6,4 +6,6 @@ const headline = document.querySelector('.headline');
 
 const tl = new TimelineMax();
 
-tl.fromTo(mint,1, {height: "0%"}, {height: '80^'})
+tl.fromTo(mint, 1, {height: "0%" }, {height: '80%', ease: Power2.easeInOut })
+.fromTo(mint, 1.2, {width: "100%"}, {width: "80%", ease: Power2.easeInOut })
+.fromTo(slider, 1.2, { x: "-100%"}, { x: '0%' ,ease: Power2.easeInOut});
